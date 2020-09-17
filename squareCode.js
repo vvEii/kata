4883e010ca867992fc5944d5cc2ba50d305afd88
shangguanwei <coopershang125@gmail.com>
2020-09-17 23:11:37 +0000

    first commit

---
+++ squareCode.js
@@ -0,0 +1,14 @@
+const squareCode = function(message) {
+  
+};
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
+
+//expected output:
+//clu hlt io
+//fto ehg ee dd
+//hae and via ecy
+//imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
